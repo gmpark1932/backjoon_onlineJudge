ae9ae4798ae2eba024b3f3c45d90a4253bb8df77
gyungmin <wnop123@gmail.com>
2017-07-09 14:03:01 +0900

    Add files via upload

---
+++ [Sort] k-th number.cc
@@ -0,0 +1,25 @@
+// Problem Number: 11004						 
+// URL: https://www.acmicpc.net/problem/11004 
+// Date: 2017.07.08							 
+// Purpose: 1) Using stl sort
+
+#include <stdio.h>
+#include <algorithm>
+
+int arr[5000001];
+
+int main(void){
+    int n,k;
+    
+    scanf("%d %d",&n,&k);
+    
+    for(unsigned int i = 0;i<n;i++){
+        scanf("%d",arr+i);
+    }
+    
+    std::sort(arr,arr+n);
+    
+    printf("%d\n",arr[k-1]);
+    
+    return 0;
+}
